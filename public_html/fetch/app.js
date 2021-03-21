b9df20a82cf995f1e40698b74bb17bb7d356b424
Андрей Портян <60672616+portyanq@users.noreply.github.com>
2021-03-21 15:15:24 +0300

    t

--- public_html/fetch/app.js
+++ public_html/fetch/app.js
@@ -1,10 +1,10 @@
 
 const but = document.querySelector("#bt");
-but.addEventListener('click', async () => {
+but.addEventListener('click', () => {
     const inp = document.querySelector("#inp").value;
     const response = fetch(`${inp}`);
     if (response.ok) { 
-        const answer = await response.text();
+        const answer = response.text();
         console.log(answer)
       }    
 })
